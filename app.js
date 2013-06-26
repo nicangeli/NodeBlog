7c93dfdbff14d40b3421e40d53d23558365d8629
Nicholas Angeli <nicangeli@gmail.com>
2013-06-26 14:54:36 +0100

    uses template to display blog posts

--- app.js
+++ app.js
@@ -34,9 +34,13 @@ var articleProvider = new ArticleProvider();
 
 app.get('/', function(req, res) {
 	articleProvider.findAll(function(error, docs){
-		res.send(docs);
-	});
+		res.render('index.jade', {
+				title: 'Blog',
+				articles: docs
+		});
+	})
 });
+
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
